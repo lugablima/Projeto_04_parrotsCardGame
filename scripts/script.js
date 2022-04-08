7bacba7a8c17536db772534fa00409defe0ce7ba
LucasLima <lugablima@gmail.com>
2022-04-08 14:04:53 -0300

    Versão 4 - Verifica se as cartas são iguais implementado

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+let cardsSelecionados = []; 
 let quantidadeCartas = Number(prompt("Com quantas cartas você quer jogar?"));
 
 while (((quantidadeCartas % 2) !== 0) || (quantidadeCartas < 4) || (quantidadeCartas > 14)) {
@@ -31,7 +32,7 @@ function distribuirCartas() {
 
     for(let k = 0; k < quantidadeCartas; k++) {
         elemento.innerHTML += `
-        <div class="card">
+        <div class="card" onclick="virarCarta(this)">
             <div class="frente face">
                 <img src="./images/front.png"/>
             </div>
@@ -45,10 +46,46 @@ function distribuirCartas() {
     elemento.classList.remove("oculto");
 } 
 
-// function virarCarta(elemento) {
+function virarCarta(elemento) {
+    let card = {
+        cardElement: elemento,
+        source: elemento.querySelector(".verso > img").attributes.src.value
+    }
 
+    card.cardElement.classList.add("selecionado");
 
-// }
+    cardsSelecionados.push(card);
+
+    console.log(cardsSelecionados.length);
+    if(cardsSelecionados.length === 2) {
+        console.log("Entrei no primeiro if");
+        if(cardsSelecionados[0].source === cardsSelecionados[1].source) {
+            console.log("Entrei no segundo if");
+            for(let i = 0; i < cardsSelecionados.length; i++) {
+                cardsSelecionados[i].cardElement.classList.add("acertou");
+                cardsSelecionados[i].cardElement.classList.remove("selecionado");
+            }
+            cardsSelecionados = [];
+        } else {
+            console.log("Entrei no else");
+            setTimeout(desvirarCarta, 1000);
+        }
+    } 
+    // else if(cardsSelecionados.length > 2) {
+
+    // }
+}
+
+function desvirarCarta() {
+    // let selecionado = document.querySelectorAll(".selecionado");
+
+    for(let j = 0; j < cardsSelecionados.length; j++) {
+        cardsSelecionados[j].cardElement.classList.remove("selecionado");
+    }
+
+    cardsSelecionados = [];
+}
+
 
 function comparador() { 
 	return Math.random() - 0.5; 
