7302f745c8689ada7367ed67f7537762451edfca
LucasLima <lugablima@gmail.com>
2022-04-10 15:31:20 -0300

    Versão 5 - Requisitos implementados

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
 let cardsSelecionados = []; 
+let quantJogadas = 0;
+// let tempoSeg = 0;
 let quantidadeCartas = Number(prompt("Com quantas cartas você quer jogar?"));
 
 while (((quantidadeCartas % 2) !== 0) || (quantidadeCartas < 4) || (quantidadeCartas > 14)) {
@@ -52,32 +54,33 @@ function virarCarta(elemento) {
         source: elemento.querySelector(".verso > img").attributes.src.value
     }
 
-    card.cardElement.classList.add("selecionado");
+    if((cardsSelecionados.length < 2) && (elemento.classList.contains("acertou") === false) && (elemento.classList.contains("selecionado") === false)) {
+        card.cardElement.classList.add("selecionado");
+        cardsSelecionados.push(card);
+        quantJogadas++;
 
-    cardsSelecionados.push(card);
-
-    console.log(cardsSelecionados.length);
-    if(cardsSelecionados.length === 2) {
-        console.log("Entrei no primeiro if");
-        if(cardsSelecionados[0].source === cardsSelecionados[1].source) {
-            console.log("Entrei no segundo if");
-            for(let i = 0; i < cardsSelecionados.length; i++) {
-                cardsSelecionados[i].cardElement.classList.add("acertou");
-                cardsSelecionados[i].cardElement.classList.remove("selecionado");
+        console.log(cardsSelecionados.length);
+        if(cardsSelecionados.length === 2) {
+            console.log("Entrei no primeiro if");
+            if(cardsSelecionados[0].source === cardsSelecionados[1].source) {
+                console.log("Entrei no segundo if");
+                for(let i = 0; i < cardsSelecionados.length; i++) {
+                    cardsSelecionados[i].cardElement.classList.add("acertou");
+                    cardsSelecionados[i].cardElement.classList.remove("selecionado");
+                }
+                cardsSelecionados = [];
+                contarCartasViradas();
+            } else {
+                console.log("Entrei no else");
+                setTimeout(desvirarCarta, 1000);
             }
-            cardsSelecionados = [];
-        } else {
-            console.log("Entrei no else");
-            setTimeout(desvirarCarta, 1000);
-        }
-    } 
-    // else if(cardsSelecionados.length > 2) {
-
-    // }
+        } 
+    }
 }
 
 function desvirarCarta() {
     // let selecionado = document.querySelectorAll(".selecionado");
+    console.log("Entrei no setTimeout");
 
     for(let j = 0; j < cardsSelecionados.length; j++) {
         cardsSelecionados[j].cardElement.classList.remove("selecionado");
@@ -90,3 +93,29 @@ function desvirarCarta() {
 function comparador() { 
 	return Math.random() - 0.5; 
 }
+
+function contarCartasViradas() {
+    let lista = document.querySelectorAll(".acertou");
+
+    if(lista.length === quantidadeCartas) {
+        fimDeJogo();
+    } else {
+        lista = [];
+    }
+}
+
+function fimDeJogo() {
+    alert(`Você ganhou em ${quantJogadas} jogadas!`);
+}
+
+// function iniciarCronômetro() {
+//     elemento = document.querySelector(".cronometro");
+//     elemento.innerHTML = tempo;
+//     setInterval(incrementarTempo, 1000);
+// }
+
+// function incrementarTempo() {
+//     elemento = document.querySelector(".cronometro");
+//     tempoSeg++;
+//     elemento.innerHTML = tempoSeg;
+// }
